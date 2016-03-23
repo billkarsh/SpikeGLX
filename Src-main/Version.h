ad1eba56351891c368a354152b6d018175167cab
billKarsh <karshb@janelia.hhmi.org>
2016-03-23 15:32:14 -0400

    Increment version

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APPNAME         "SpikeGLX"
-#define VERSION         0x20160301
-#define VERSION_STR     "SpikeGLX v.20160301, Imec v3.0"
+#define VERSION         0x20160302
+#define VERSION_STR     "SpikeGLX v.20160302, Imec v3.0"
 
 #endif  // VERSION_H
 
