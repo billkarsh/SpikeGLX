dfe121c4c96d92e644a55ca09ad00b5f04503053
billKarsh <karshb@janelia.hhmi.org>
2017-08-27 18:00:01 -0400

    Advance version: pushing multistream support into 3A

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APPNAME         "SpikeGLX"
-#define VERSION         0x20170814
-#define VERSION_STR     "SpikeGLX v.20170814, Imec v4.3"
+#define VERSION         0x20170827
+#define VERSION_STR     "SpikeGLX v.20170827, Imec v4.3"
 
 #endif  // VERSION_H
 
