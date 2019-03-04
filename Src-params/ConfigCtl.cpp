a31b69c7647cc349dee0eeb70908a85cbea5a605
billKarsh <karshb@janelia.hhmi.org>
2019-03-04 17:28:54 -0500

    Typo

--- Src-params/ConfigCtl.cpp
+++ Src-params/ConfigCtl.cpp
@@ -1601,7 +1601,7 @@ void ConfigCtl::sourceMakeName()
     if( nisrc.exttrig ) {
 
         if( sourceUI->whisperRadio->isChecked() )
-            name += "_whipser";
+            name += "_whisper";
         else
             name += "_ext";
     }
