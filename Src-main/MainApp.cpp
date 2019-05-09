4c3b65ebb0362a0281f2e04007ae05aa32a09806
billKarsh <karshb@janelia.hhmi.org>
2019-05-09 14:55:19 -0400

    Better parsing of file paths

--- Src-main/MainApp.cpp
+++ Src-main/MainApp.cpp
@@ -543,7 +543,7 @@ void MainApp::options_PickDataDir()
 
 void MainApp::options_ExploreRunDir()
 {
-    QDesktopServices::openUrl( QUrl( appData.dataDir ) );
+    QDesktopServices::openUrl( QUrl::fromUserInput( appData.dataDir ) );
 }
 
 
@@ -730,7 +730,7 @@ void MainApp::help_HelpDlg()
 
 void MainApp::help_ExploreApp()
 {
-    QDesktopServices::openUrl( QUrl( appPath() ) );
+    QDesktopServices::openUrl( QUrl::fromUserInput( appPath() ) );
 }
 
 
