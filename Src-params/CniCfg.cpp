fabadba83ed5a5a4ee1083a320f666822199f907
billKarsh <karshb@janelia.hhmi.org>
2019-12-17 19:53:13 -0500

    Fix NI DI and PFI channel counting

--- Src-params/CniCfg.cpp
+++ Src-params/CniCfg.cpp
@@ -843,7 +843,7 @@ static bool isPFI2DI( const QString &dev, const QString &pfi )
 
     ret = DAQmxTaskControl( task, DAQmx_Val_Task_Commit );
 
-    if( DAQmxFailed( ret ) )
+    if( DAQmxFailed( ret ) && ret != DAQmxErrorTrigLineNotFoundSingleDevRoute_Routing )
         goto exit;
 
     ok = true;
@@ -1272,7 +1272,7 @@ int CniCfg::nWaveformLines( const QString &dev )
 
         ret = DAQmxTaskControl( task, DAQmx_Val_Task_Commit );
 
-        if( DAQmxFailed( ret ) )
+        if( DAQmxFailed( ret ) && ret != DAQmxErrorExtSampClkSrcNotSpecified )
             goto next_line;
 
         ok = true;
