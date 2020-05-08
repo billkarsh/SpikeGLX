445a90e5aa688b2aaf3fd9f75a4e0fbce98a05a7
billKarsh <karshb@janelia.hhmi.org>
2020-05-08 02:48:37 -0400

    Doc typo

--- MATLAB-SDK/@SpikeGL/SetNextFileName.m
+++ MATLAB-SDK/@SpikeGL/SetNextFileName.m
@@ -8,7 +8,7 @@
 %     is under full user control. SpikeGLX clears your custom
 %     name and reverts to auto-naming after the current files
 %     are written. You must, therefore, call this function for
-%     each file, set and you must set the name before a trigger
+%     each file set, and you must set the name before a trigger
 %     event for that file set.
 %
 function [s] = SetNextFileName( s, name )
