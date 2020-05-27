d94bf3bee2e77b30d7c16dcf4212b88f3cbb8e72
billKarsh <karshb@janelia.hhmi.org>
2020-05-27 19:24:49 -0400

    Advance version 20200513

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APPNAME         "SpikeGLX"
-#define VERSION         0x20200309
-#define VERSION_STR     "SpikeGLX v.20200309, Imec phase20 v2.11"
+#define VERSION         0x20200513
+#define VERSION_STR     "SpikeGLX v.20200513, Imec phase30 v3.13"
 
 #endif  // VERSION_H
 
