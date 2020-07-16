6424a2812b85797413fb8e9005476c750697924e
billKarsh <karshb@janelia.hhmi.org>
2020-07-16 18:06:33 -0400

    Undo version 20200520

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APPNAME         "SpikeGLX"
-#define VERSION         0x20200710
-#define VERSION_STR     "SpikeGLX v.20200710, Imec phase3B2 v1.20"
+#define VERSION         0x20200520
+#define VERSION_STR     "SpikeGLX v.20200520, Imec phase3B2 v1.20"
 
 #endif  // VERSION_H
 
