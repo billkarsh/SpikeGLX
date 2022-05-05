d1c7c5764d8e7645e5f08e69f00fae197730dbb3
billKarsh <karshb@janelia.hhmi.org>
2022-05-05 16:31:43 -0400

    Advance version 20220101

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define APPNAME         "SpikeGLX"
-#define VERSION         0x20210122
-#define VERSION_STR     "SpikeGLX v.20210122, Imec phase30 v3.43"
+#define VERSION         0x20220101
+#define VERSION_STR     "SpikeGLX v.20220101, Imec phase30 v3.43"
 
 #endif  // VERSION_H
 
