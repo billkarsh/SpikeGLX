92fd7fc28656046c7953f25918d8695ae771222a
billKarsh <karshb@janelia.hhmi.org>
2023-04-25 12:03:50 -0700

    API 3.60

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -3,7 +3,7 @@
 
 #define APPNAME         "SpikeGLX"
 #define VERS_SGLX       0x20230411
-#define VERS_SGLX_STR   "SpikeGLX v.20230411, Imec phase30 v3.59"
+#define VERS_SGLX_STR   "SpikeGLX v.20230411, Imec phase30 v3.60"
 
 #define VERS_IMEC_BS    "2.0.169"
 #define VERS_IMEC_BSC   "3.2.186"
