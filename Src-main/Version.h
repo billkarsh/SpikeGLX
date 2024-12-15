0c8d0c52a5ef2c0075e71b6359e97952c0390424
billKarsh <karshb@janelia.hhmi.org>
2024-12-14 18:29:15 -0800

    Advance version 20241215

--- Src-main/Version.h
+++ Src-main/Version.h
@@ -2,12 +2,12 @@
 #define VERSION_H
 
 #define APPNAME         "SpikeGLX"
-#define VERS_SGLX       0x20241001
+#define VERS_SGLX       0x20241215
 
 #ifdef HAVE_NXT
-#define VERS_SGLX_STR   "SpikeGLX v.20241001, NXT v3.69"
+#define VERS_SGLX_STR   "SpikeGLX v.20241215, NXT v3.69"
 #else
-#define VERS_SGLX_STR   "SpikeGLX v.20241001, Imec phase30 v3.70.3"
+#define VERS_SGLX_STR   "SpikeGLX v.20241215, Imec phase30 v3.70.3"
 #endif
 
 #define VERS_IMEC_BS    "2.0.169"
